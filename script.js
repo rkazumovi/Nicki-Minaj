8ea3673cd313feedae006695ebc1321634f09277
rkazumovi <rasuliqazumovi@gmail.com>
2023-04-01 15:55:11 +0400

    do project

---
+++ script.js
@@ -0,0 +1,5 @@
+let navigation = document.getElementById("navbar");
+let burger = document.getElementById('burgerbar');
+burger.addEventListener('click', function () {
+    navigation.classList.toggle('activeNav')
+})
